60f451b996ded40b23088d9e964e4b7e977af857
fgittins <fabiangittins@gmail.com>
2023-12-02 13:08:24 +0000

    Successful attempts for Day 2

---
+++ day2/part1.jl
@@ -0,0 +1,33 @@
+lines = readlines("day2/input.txt")
+
+poss_ID = 0
+for line in lines
+    new_line = split(line, ": ")
+    ID = parse(Int, split(new_line[1], " ")[end])
+    handfuls = split(new_line[2], "; ")
+
+    poss = true
+    for handful in handfuls
+        red_num = green_num = blue_num = 0
+        colours_in_handful = split(handful, ", ")
+        for string in colours_in_handful
+            num, colour = split(string, " ")
+            if "red" == colour
+                red_num = parse(Int, num)
+            elseif "green" == colour
+                green_num = parse(Int, num)
+            elseif "blue" == colour
+                blue_num = parse(Int, num)
+            end
+        end
+        if red_num > 12 || green_num > 13 || blue_num > 14
+            poss = false
+            break
+        end
+    end
+
+    if poss
+        global poss_ID += ID
+    end
+end
+poss_ID
---
+++ day2/part2.jl
@@ -0,0 +1,27 @@
+lines = readlines("day2/input.txt")
+
+sum_of_power = 0
+for line in lines
+    new_line = split(line, ": ")
+    ID = parse(Int, split(new_line[1], " ")[end])
+    handfuls = split(new_line[2], "; ")
+
+    red_min = green_min = blue_min = 0
+    for handful in handfuls
+        colours_in_handful = split(handful, ", ")
+        for string in colours_in_handful
+            num, colour = split(string, " ")
+            num = parse(Int, num)
+            if "red" == colour && num > red_min
+                red_min = num
+            elseif "green" == colour && num > green_min
+                green_min = num
+            elseif "blue" == colour && num > blue_min
+                blue_min = num
+            end
+        end
+    end
+
+    global sum_of_power += red_min * green_min * blue_min
+end
+sum_of_power
