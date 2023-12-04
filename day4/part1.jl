5dfb389c4e0763113d38973c43eeb40a69f809fe
fgittins <fabiangittins@gmail.com>
2023-12-04 22:23:25 +0000

    Successful attempts at Day 4

---
+++ day4/part1.jl
@@ -0,0 +1,30 @@
+function calculate_points_from_card(line)
+    new_line1 = split(line, ": ")
+    new_line2 = split(new_line1[2], " | ")
+    winning_nums = split(new_line2[1])
+    nums_you_have = split(new_line2[2])
+
+    total_winning_nums = 0
+    for num in nums_you_have
+        if num in winning_nums
+            total_winning_nums += 1
+        end
+    end
+    points = 0
+    if total_winning_nums >= 1
+        points = 2^(total_winning_nums - 1)
+    end
+    points
+end
+
+function calculate_total_points(lines)
+    total_points = 0
+    for line in lines
+        points = calculate_points_from_card(line)
+        total_points += points
+    end
+    total_points
+end
+
+lines = readlines("day4/input.txt")
+calculate_total_points(lines)
---
+++ day4/part2.jl
@@ -0,0 +1,34 @@
+function calculate_matching_nums_from_card(line)
+    new_line1 = split(line, ": ")
+    new_line2 = split(new_line1[2], " | ")
+    winning_nums = split(new_line2[1])
+    nums_you_have = split(new_line2[2])
+
+    total_matching_nums = 0
+    for num in nums_you_have
+        if num in winning_nums
+            total_matching_nums += 1
+        end
+    end
+    total_matching_nums
+end
+
+function calculate_card_instances(lines)
+    n = length(lines)
+    instances = ones(Int64, n)
+    for i = 1:n
+        line = lines[i]
+        matching_nums = calculate_matching_nums_from_line(line)
+        if matching_nums != 0
+            for instance = 1:instances[i]
+                for j = i+1:i+matching_nums
+                    instances[j] += 1
+                end
+            end
+        end
+    end
+    instances
+end
+
+lines = readlines("day4/input.txt")
+sum(calculate_card_instances(lines))
